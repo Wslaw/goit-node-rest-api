05d47a10d1b7525e54363a55c14ea4e4c8bfd573
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-03-19 20:36:39 +0200

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,6 @@
 import { listContacts, getContactById, addContact, removeContact, upgradeContact } from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
-import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
+// import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
 
 export const getAllContacts = async (req, res, next) => {
   try {
@@ -38,33 +38,23 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-    const { error } = createContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-      const { name, email, phone } = req.body;
+    const { name, email, phone } = req.body;
     const result = await addContact(name, email, phone);
     res.status(201).json(result);
   } catch (error) {
     next(error);
-  }};
+  }
+};
 
-export const updateContact = async(req, res, next) => {
-    try {
-        const { error } = updateContactSchema.validate(req.body);
-        if (error) {
-            throw HttpError(400, error.message);
-        }
-        const { id } = req.params;
-        const result = await upgradeContact(id, req.body);
-        console.log(result);
-        
-        if (!result) {
-            throw HttpError(404);
-        }
-        res.status(200).json(result);
-
-    } catch (error) {
-        next(error)
+export const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await upgradeContact(id, req.body);
+    if (!result) {
+      throw HttpError(404);
     }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
