6a2355f290cde9af642a87be400825f22ee3a089
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-04-04 19:54:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
-// app.use("/api/user", authRouter);
+// app.use("/api/auth", authRouter);
+app.use("/api/user", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
