f6e69ba11769ebfd08ff5f653555a3650fa9d287
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-04-04 17:23:44 +0300

    app

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-app.use("/api/user", authRouter);
+// app.use("/api/user", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,13 +1,12 @@
 import express from "express";
 
-import validateBody from "../decorators/validateBody.js";
+import validateBody from "../helpers/validateBody.js";
 
 import { registerSchema, loginSchema } from "../schemas/usersSchemas.js";
 
-import authControllers from "../controllers/authControllers.js";
+import { login, register, logout, getCurrent } from "../controllers/authControllers.js";
 import authenticate from "../middlewares/authenticate.js";
 
-const { register, login, logout, getCurrent } = authControllers;
 
 const authRouter = express.Router();
 
